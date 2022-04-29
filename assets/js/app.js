230d1678bee7923e48a6db3b7c431dfe558389e1
GabriPini <lele19977@gmail.com>
2022-04-29 13:38:21 +0200

    initial commit and final commit

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+const app = new Vue(
+    {
+        el : '#app',
+        data : {
+            emailList:[],
+        },
+        
+        method: {
+    
+        },
+        
+        mounted(){
+            for (let i=0; i<10; i++){
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(response =>{
+                this.emailList.push(response.data.response);
+               
+            })
+        }
+
+        },
+
+
+    });
+
+ 
